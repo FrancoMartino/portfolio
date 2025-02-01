6461b20206e09de0cea5360e20e841ac112d296f
FrancoMartino <martinofrancoezequiel@gmail.com>
2025-02-01 17:32:45 -0300

    Update Tag.tsx

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -10,12 +10,11 @@ interface Interface {
 export default function Tag(attributes: Interface) {
   return (
     <a
-      href={attributes.link}
+      {...(attributes.link ? { href: attributes.link, target: "_blank" } : {})}
       className={
         "relative group inline-flex items-center select-none gap-2 p-2 px-4 rounded-full transition-transform duration-300 bg-gray-700 text-white shadow-lg shadow-gray-700/30 hover:scale-[102%] " +
         attributes.className
       }
-      target="_blank"
     >
       <div className="relative flex items-center gap-2">
         {attributes.icon && (
