287308afca73e1a02b57ea9a6936fc3ac7a8849f
Franco Martino <95370336+FrancoMartino@users.noreply.github.com>
2025-06-10 18:40:13 -0300

    Update Navbar.tsx

--- src/components/sections/Presentation/Navbar.tsx
+++ src/components/sections/Presentation/Navbar.tsx
@@ -1,22 +1,9 @@
 import React from "react";
 import Contact from "../../common/SocialMedia";
 
-interface NavbarLinkProps {
-  href: string;
-  children: React.ReactNode;
-}
-
-const NavbarLink: React.FC<NavbarLinkProps> = ({ href, children }) => (
-  <a
-    href={href}
-    className="flex flex-row gap-2 text-gray-300 hover:text-white hover:underline"
-  >
-    {children}
-  </a>
-);
 
 const Navbar: React.FC = () => (
-  <div className="lg:flex lg:absolute h-min inset-0 p-8 hidden text-gray-300 justify-end">
+  <div className="lg:flex lg:absolute h-min inset-0 p-8 hidden text-gray-300 justify-end z-10">
     <Contact />
   </div>
 );
